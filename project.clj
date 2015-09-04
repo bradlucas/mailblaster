088d8f7bcde29214caa02c5e4aca8a9952592735
Brad Lucas <brad@beaconhill.com>
2015-09-04 09:23:37 -0400

    updated versions

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject org.clojars.blucas/mailblaster "1.0.0"
   :description "Clojure Mass Mailing Application"
   :dependencies
-  [[org.clojure/clojure "1.3.0"]
-   [org.clojure/tools.cli "0.2.1"]
-   ;;   [com.draines/postal "1.7-SNAPSHOT"]          ;; need the 1.8 version to work properly, not yet on clojars
-   [org.clojars.blucas/postal "1.8-SNAPSHOT"]
+  [[org.clojure/clojure "1.7.0"]
+   [org.clojure/tools.cli "0.3.3"]
+   [com.draines/postal "1.11.3"]
    ]
   :main mailblaster.core)
