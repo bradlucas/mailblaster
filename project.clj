68edb8b6b15e4ace8d32a257c86db9eb37bb421a
Brad Lucas <brad@beaconhill.com>
2020-11-15 22:36:37 -0500

    new group com.bradlucas

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.blucas/mailblaster "1.0.1"
+(defproject com.bradlucas/mailblaster "1.0.1"
   :description "Clojure Mass Mailing Application"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
